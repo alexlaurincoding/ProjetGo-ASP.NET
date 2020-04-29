c76616eeb6fb97480d0810a17396678eeb42c135
tsevedire <63421776+tsevedire@users.noreply.github.com>
2020-04-29 13:00:11 -0400

    paypal config pas fini

---
+++ ProjetGoEquipe2/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetGoEquipe2.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult PaymentWithPaypal()
+        {
+            APIContext apiContext = PaypalConfiguration.GetAPIContext();
+            try
+            {
+
+                string PayerID = Request.Params["PayerID"];
+                if (string.IsNullOrEmpty(PayerID))
+                {
+                    string baseURL = Request.Url.Scheme + "://" + Request.Url.Authority + "PaymentWithPaypal/PaymentWithPaypal?";
+                    var Guid = Convert.ToString((new Random()).Next(100000000));
+                    var createPayment = this.CreatePayment(apiContext, baseURL + "guid=" + Guid);
+                }
+            }
+            catch (Exception)
+            {
+
+               // throw;
+            }
+            //return View();
+        }
+
+        private Cotisation CreatePayment(APIContext apiContext, string redirectURL)
+        {
+            
+        }
+    }
+}
