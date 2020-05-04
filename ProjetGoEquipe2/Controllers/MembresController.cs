3040bad628dbd962ca64685b205c5d2b2bbdd60e
tsevedire <63421776+tsevedire@users.noreply.github.com>
2020-05-04 16:36:54 -0400

    Boutton paypal tester et fonctionnel

--- ProjetGoEquipe2/Controllers/MembresController.cs
+++ ProjetGoEquipe2/Controllers/MembresController.cs
@@ -58,6 +58,12 @@ namespace ProjetGoEquipe2.Controllers
             return View();
         }
 
+        public ActionResult RenouvAbonnementCancel()
+        {
+            //code pour resetter l'abonnement du client
+            return View();
+        }
+
         // GET: Membre/Identifier
         public ActionResult Identifier()
         {
