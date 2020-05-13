667cb5f0fc2bd55f90ac237ab6793a0d33d0d365
girouxcoding <48444806+GCoding83@users.noreply.github.com>
2020-05-13 09:09:35 -0400

    J'avais essayé une image de bacground. Je sais pas si on veut garder çaé

---
+++ ProjetGoEquipe2/Controllers/UtilsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetGoEquipe2.Controllers
+{
+    public class UtilsController : Controller
+    {
+        // GET: Utils
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+    }
+}
